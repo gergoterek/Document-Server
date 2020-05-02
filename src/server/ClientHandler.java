ffa845adf7809fb1c4f823631053d89e8fc97b58
gergoterek <gergoterek@gmail.com>
2020-05-02 20:03:41 +0200

    Server and Cliendhandler fundamentals with iterative implementation

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,4 +1,125 @@
 package server;
 
-public class ClientHandler implements IClientHandler{
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class ClientHandler implements IClientHandler, AutoCloseable, Runnable{
+
+    private Socket s;
+    private BufferedReader bf;
+    private PrintWriter pw;
+    private LinkedHashSet<String> contents;
+
+    public ClientHandler(ServerSocket ss, LinkedHashSet<String> contents) throws Exception{
+        s = ss.accept();
+        bf = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        pw = new PrintWriter(s.getOutputStream());
+        this.contents = contents;
+    }
+    public void close() throws Exception {
+        if (s != null) {
+            pw.flush();
+            s.close();
+        }
+    }
+    public void handleDownloadDocument(BufferedReader fromClient, PrintWriter toClient) throws IOException {
+        bf = fromClient;
+        pw.println("Give a doc name:");
+        String fileName = fromClient.readLine();
+        synchronized (contents) {
+            if (contents.contains(fileName)) {
+                try (
+                    Scanner scFile = new Scanner(new File(fileName));
+                ) {
+                    while (scFile.hasNextLine()) {
+                        pw.println(scFile.nextLine());
+                    }
+                    pw.println("END_OF_DOCUMENT");
+                }
+            } else {
+                toClient.println("NOT_FOUND");
+            }
+        }
+        toClient.flush();
+        fromClient.close();
+    }
+    public void handleUploadDocument(BufferedReader fromClient, PrintWriter toClient) throws IOException {
+        pw.println("Give a new doc name:");
+        String fileName = fromClient.readLine();
+        ArrayList<String> fileText = new ArrayList<>();
+
+        //Dokumentum szerkesztése
+        if(contents.contains(fileName)){
+            synchronized (contents) {
+                String line;
+                while ((line = fromClient.readLine()) != null) {
+                    if (!line.equals("END_OF_DOCUMENT")) {
+                        fileText.add(line);
+                    } else {
+                        break;
+                    }
+                }
+                FileWriter fileWriter = new FileWriter(fileName);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                for(String l: fileText) {
+                    printWriter.print(l);
+                }
+            }
+        } else { //Új dokumentum
+            String line;
+            while ((line = fromClient.readLine()) != null) {
+                if (!line.equals("END_OF_DOCUMENT")) {
+                    fileText.add(line);
+                } else {
+                    break;
+                }
+            }
+        }
+        FileWriter fileWriter = new FileWriter(fileName);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        for(String l: fileText) {
+            printWriter.print(l);
+        }
+        printWriter.close();
+        contents.add(fileName);
+        toClient.println("END_OF_DOCUMENT");
+        fromClient.close();
+
+    }
+    public void handleListDocuments(PrintWriter toClient){
+        for(String name: contents){
+            toClient.println(name);
+        }
+        toClient.println("END_OF_LIST");
+    }
+    public void handleUnknownRequest(PrintWriter toClient) throws IOException{
+        toClient.flush();
+        s.close();
+    }
+    public void run(){
+        try {
+            String line;
+            while ((line = this.bf.readLine()) != null) {
+                switch (line) {
+                    case "DOWNLOAD_DOCUMENT":
+                        handleDownloadDocument(bf, pw);
+                        break;
+                    case "UPLOAD_DOCUMENT":
+                        handleUploadDocument(bf, pw);
+                        break;
+                    case "LIST_DOCUMENTS":
+                        handleListDocuments(pw);
+                        break;
+                    default:
+                        handleUnknownRequest(pw);
+                        break;
+                }
+            }
+        } catch (IOException e){}
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,56 @@
 package server;
 
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) throws Exception {
+        LinkedHashSet<String> index = new LinkedHashSet<>();
+        for (int i = 0; i < args.length; i++) {
+            index.add(args[i]);
+        }
+
+        int port = 50000;
+        //try (
+        ServerSocket ss = new ServerSocket(port);
+
+        ClientHandler client1 = new ClientHandler(ss, index);
+        ClientHandler client2 = new ClientHandler(ss, index);
+
+        while (true) {
+            client1.run();
+            client2.run();
+        }
+
+
+//                Socket s1 = ss.accept();
+//                Scanner sc1 = new Scanner(s1.getInputStream());
+//                PrintWriter pw1 = new PrintWriter(s1.getOutputStream());
+//
+//                Socket s2 = ss.accept();
+//                Scanner sc2 = new Scanner(s2.getInputStream());
+//                PrintWriter pw2 = new PrintWriter(s2.getOutputStream());
+        //){
+//            while (sc1.hasNextLine()) {
+//                var line1 = sc1.nextLine();
+//                pw2.println(line1);
+//                pw2.flush();
+//
+//                if (!sc2.hasNextLine())   break;
+//                var line2 = sc2.nextLine();
+//                pw1.println(line2);
+//                pw1.flush();
+//            }
+//            if (s1 != null) {
+//                s1.close();
+//            }
+//            if (s2 != null) {
+//                s2.close();
+//            }
+    }
 }
+
+
